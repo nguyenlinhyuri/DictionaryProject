18d8199c9912aeb1307e4a03d2e384f895dd553b
NguyenLinh <118706598+nguyenlinhyuri@users.noreply.github.com>
2023-10-03 09:36:33 +0700

    commandline

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        DictionaryCommand_Line dict = new DictionaryCommand_Line();
+        dict.dictionaryAdvanced();
+    }
+}
---
+++ src/Word.java
@@ -0,0 +1,41 @@
+public class Word {
+    /**
+     * word_target: english word
+     * word_explain: vietnamese explain
+     */
+    private String word_target;
+    private String word_explain;
+
+    /**
+     * constructor
+     * @param word_target is english
+     * @param word_explain is vietnamese
+     */
+    public Word(String word_target, String word_explain) {
+        this.word_target = word_target;
+        this.word_explain = word_explain;
+    }
+
+    boolean check_start(String s){
+        for (int i=0; i<s.length(); i++){
+            if (s.charAt(i) != this.word_target.charAt(i)) return false;
+        }
+        return true;
+    }
+
+    public String getWord_target() {
+        return word_target;
+    }
+
+    public void setWord_target(String word_target) {
+        this.word_target = word_target;
+    }
+
+    public String getWord_explain() {
+        return word_explain;
+    }
+
+    public void setWord_explain(String word_explain) {
+        this.word_explain = word_explain;
+    }
+}
